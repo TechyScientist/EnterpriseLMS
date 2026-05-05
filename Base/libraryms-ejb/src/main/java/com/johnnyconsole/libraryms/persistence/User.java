cda93dc5b4ccdc40851130ab1e39c0792dd491ff
Johnny Console <johnny.console@hotmail.com>
2026-05-05 16:56:46 -0400

    Create POJO for User entities

---
+++ Base/libraryms-ejb/src/main/java/com/johnnyconsole/libraryms/persistence/User.java
@@ -0,0 +1,48 @@
+package com.johnnyconsole.libraryms.persistence;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="libraryms_users")
+@NamedQueries({
+        @NamedQuery(name="User.FindByBarcode", query="SELECT u FROM User u WHERE u.barcode=:barcode"),
+        @NamedQuery(name="User.FindByUsername", query="SELECT u FROM User u WHERE u.username=:username"),
+        @NamedQuery(name="User.SearchByLastName", query="SELECT u FROM User u WHERE u.lastName LIKE '%:criteria%'")
+})
+public class User {
+
+    @Id
+    public String barcode;
+    public String username, lastName, firstName;
+    public double balance;
+    public boolean libraryStaff, libraryAdmin;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String barcode, String username, String password, String lastName, String firstName) {
+        this.barcode = barcode;
+        this.username = username;
+        this.password = password;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //TODO: Remove this method -- used only for testing
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("User {\n\tbarcode: ").append(barcode)
+                .append("\n\tlastName: ").append(lastName)
+                .append("\n\tfirstName: ").append(firstName)
+                .append("\n\tusername: ").append(username)
+                .append("\n\tbalance: ").append(String.format("$%.2f", balance))
+                .append("\n}").toString();
+    }
+}
