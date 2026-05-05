4c14fd8dd202fd561389471b6a6f2bd21b075fc4
Johnny Console <johnny.console@hotmail.com>
2026-05-05 16:58:39 -0400

    Interface with the database to verify user credentials

--- Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/SignInServlet.java
+++ Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/SignInServlet.java
@@ -1,23 +1,53 @@
 package com.johnnyconsole.libraryms.servlet;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.johnnyconsole.libraryms.persistence.User;
+import com.johnnyconsole.libraryms.persistence.interfaces.UserDao;
+
+import javax.ejb.EJB;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static javax.servlet.http.HttpServletResponse.*;
 
 @WebServlet("SignInServlet")
 public class SignInServlet extends HttpServlet {
 
+    @EJB
+    private UserDao userDao;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
         if(request.getParameter("signin-submit") != null) {
             String username = request.getParameter("username"),
                     password = request.getParameter("password");
-                //request.getSession().setAttribute("status", SC_UNAUTHORIZED);
-                request.getSession().setAttribute("play-sound", "");
-                response.sendRedirect("/libraryms/dashboard.jsp");
+
+            if (!(username.isEmpty() && password.isEmpty())) {
+                User user = userDao.getUserByUsername(username);
+                if (user != null) {
+                    if (BCrypt.verifyer(BCrypt.Version.VERSION_2A).verifyStrict(password.toCharArray(), user.getPassword().toCharArray())
+                            .verified) {
+                        session.setAttribute("user", user);
+                        session.setAttribute("play-sound", "");
+                        response.sendRedirect("/libraryms/dashboard.jsp");
+                    }
+                } else {
+                    session.setAttribute("status", SC_NOT_FOUND);
+                    response.sendRedirect("/libraryms/signin.jsp");
+                }
+            }
+            else {
+                session.setAttribute("status", SC_NOT_ACCEPTABLE);
+                response.sendRedirect("/libraryms/signin.jsp");
+            }
+        }
+        else {
+            session.setAttribute("status", SC_BAD_REQUEST);
+            response.sendRedirect("/libraryms/signin.jsp");
         }
     }
 
