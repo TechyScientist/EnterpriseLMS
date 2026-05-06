599019911b0f50ea4ad54ab59fbddfbb9ac72d10
Johnny Console <johnny.console@hotmail.com>
2026-05-06 18:32:34 -0400

    Ensure copy/title barcodes are valid and start with the proper values

--- Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/CatalogSearchServlet.java
+++ Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/CatalogSearchServlet.java
@@ -1,5 +1,6 @@
 package com.johnnyconsole.libraryms.servlet;
 
+import com.johnnyconsole.libraryms.interfaces.BarcodeBean;
 import com.johnnyconsole.libraryms.persistence.Book;
 import com.johnnyconsole.libraryms.persistence.interfaces.BookDao;
 
@@ -20,33 +21,47 @@ public class CatalogSearchServlet extends HttpServlet {
     @EJB
     private BookDao bookDao;
 
+    @EJB
+    private BarcodeBean barcodeBean;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession();
         if(request.getParameter("search-submit") != null) {
             String searchBy = request.getParameter("searchBy");
+            session.setAttribute("criteria", searchBy);
             switch (searchBy) {
                 case "copyBarcode": {
                     String barcode = request.getParameter("copyBarcode");
-                    Book book = bookDao.findByCopyCode(barcode);
-                    if (book == null) {
-                        session.setAttribute("status", SC_NOT_FOUND);
-                        response.sendRedirect("/library/catalog-search.jsp");
+                    if(barcode.charAt(0) >= '2' && barcode.charAt(0) <= '8' && barcodeBean.isValid(barcode)) {
+                        Book book = bookDao.findByCopyCode(barcode);
+                        if (book == null) {
+                            session.setAttribute("status", SC_NOT_FOUND);
+                            response.sendRedirect("/library/catalog-search.jsp");
+                        } else {
+                            session.setAttribute("status", SC_ACCEPTED);
+                            session.setAttribute("book", book);
+                            response.sendRedirect("/library/catalog-search.jsp");
+                        }
                     } else {
-                        session.setAttribute("status", SC_ACCEPTED);
-                        session.setAttribute("book", book);
+                        session.setAttribute("status", SC_NOT_ACCEPTABLE);
                         response.sendRedirect("/library/catalog-search.jsp");
                     }
                 }
                 break;
                 case "titleBarcode": {
                     String barcode = request.getParameter("titleBarcode");
-                    List<Book> books = bookDao.findByTitleBarcode(barcode);
-                    if (books.isEmpty()) {
-                        session.setAttribute("status", SC_NOT_FOUND);
-                        response.sendRedirect("/library/catalog-search.jsp");
+                    if(barcode.startsWith("978") && barcodeBean.isValid(barcode)) {
+                        List<Book> books = bookDao.findByTitleBarcode(barcode);
+                        if (books.isEmpty()) {
+                            session.setAttribute("status", SC_NOT_FOUND);
+                            response.sendRedirect("/library/catalog-search.jsp");
+                        } else {
+                            session.setAttribute("status", SC_ACCEPTED);
+                            session.setAttribute("booklist", books);
+                            response.sendRedirect("/library/catalog-search.jsp");
+                        }
                     } else {
-                        session.setAttribute("status", SC_ACCEPTED);
-                        session.setAttribute("booklist", books);
+                        session.setAttribute("status", SC_NOT_ACCEPTABLE);
                         response.sendRedirect("/library/catalog-search.jsp");
                     }
                 }
