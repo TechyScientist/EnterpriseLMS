dfb8b34e3c4330ff3eda84d1c78770660d1a6861
Johnny Console <johnny.console@hotmail.com>
2026-05-06 10:57:21 -0400

    Add book persistence unit

---
+++ Base/libraryms-ejb/src/main/java/com/johnnyconsole/libraryms/persistence/Book.java
@@ -0,0 +1,51 @@
+package com.johnnyconsole.libraryms.persistence;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Table(name="libraryms_books")
+@NamedQueries({
+        @NamedQuery(name="Book.SearchByCopyBarcode", query="SELECT b FROM Book b WHERE b.copyBarcode=:barcode"),
+        @NamedQuery(name="Book.SearchByTitleBarcode", query="SELECT b FROM Book b WHERE b.titleBarcode=:barcode"),
+        @NamedQuery(name="Book.SearchByTitle", query="SELECT b FROM Book b WHERE b.title LIKE '%:criteria%'"),
+        @NamedQuery(name="Book.SearchByAuthor", query="SELECT b FROM Book b WHERE b.author LIKE '%:criteria%'"),
+        @NamedQuery(name="Book.CheckedOutByPatron", query="SELECT b FROM Book b WHERE b.status='Checked Out' AND b.outTo=:patron"),
+        @NamedQuery(name="Book.ListAvailable", query="SELECT b FROM Book b WHERE b.status='Available'"),
+        @NamedQuery(name="Book.ListCheckedOut", query="SELECT b FROM Book b WHERE b.status='Checked Out'"),
+        @NamedQuery(name="Book.ListLost", query="SELECT b FROM Book b WHERE b.status='Lost'")
+})
+public class Book {
+
+    @Id
+    public String copyBarcode;
+    public String titleBarcode, author, title,status, outTo;
+    public Date dueDate;
+
+    public Book() {
+
+    }
+
+    public Book(String copyBarcode, String titleBarcode, String author, String title) {
+        this.copyBarcode = copyBarcode;
+        this.titleBarcode = titleBarcode;
+        this.author = author;
+        this.title = title;
+        this.status = "Available";
+    }
+
+    //TODO: Remove this method -- used only for testing
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append("Book {\n\tcopyBarcode: ").append(copyBarcode)
+                .append("\n\ttileBarcode: ").append(titleBarcode)
+                .append("\n\tauthor: ").append(author)
+                .append("\n\ttitle: ").append(title)
+                .append("\n\tstatus: ").append(status)
+                .append(status.equals("Checked Out") ? "\n\t\toutTo: " : "").append(status.equals("Checked Out") ? outTo : "")
+                .append(status.equals("Checked Out") ? "\n\t\tdueDate: " : "").append(status.equals("Checked Out") ? dueDate.toLocalDate().format(DateTimeFormatter.ofPattern("dd MMMM yyyy")) : "")
+                .append("\n}").toString();
+    }
+}
