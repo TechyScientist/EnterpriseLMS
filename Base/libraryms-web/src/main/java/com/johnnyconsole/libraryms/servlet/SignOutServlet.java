dba077f9145542d7dc53774cb71caa90b9488841
Johnny Console <johnny.console@hotmail.com>
2026-05-21 10:55:29 -0400

    Simply invalidate sessions on signing out

--- Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/SignOutServlet.java
+++ Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/SignOutServlet.java
@@ -4,21 +4,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("SignOutServlet")
 public class SignOutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        HttpSession session = request.getSession();
-        session.removeAttribute("user");
-        session.removeAttribute("UserDao");
-        session.removeAttribute("FineDao");
-        session.removeAttribute("BookDao");
-        session.removeAttribute("TitleDao");
-        session.removeAttribute("HoldDao");
-        session.setAttribute("signed-out", "");
+        request.getSession().invalidate();
         response.sendRedirect("/library/");
     }
 }
