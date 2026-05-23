76e7b52b3cab6824e9d9ceda2941fb4674694d01
Johnny Console <johnny.console@hotmail.com>
2026-05-23 11:39:49 -0400

    Add servlet to add a title record

---
+++ Base/libraryms-web/src/main/java/com/johnnyconsole/libraryms/servlet/AddTitleServlet.java
@@ -0,0 +1,80 @@
+package com.johnnyconsole.libraryms.servlet;
+
+import com.johnnyconsole.libraryms.interfaces.BarcodeBean;
+import com.johnnyconsole.libraryms.persistence.Book;
+import com.johnnyconsole.libraryms.persistence.Title;
+import com.johnnyconsole.libraryms.persistence.User;
+import com.johnnyconsole.libraryms.persistence.interfaces.BookDao;
+import com.johnnyconsole.libraryms.persistence.interfaces.TitleDao;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.*;
+
+@WebServlet("AddTitleServlet")
+public class AddTitleServlet extends HttpServlet {
+
+    @EJB
+    private BarcodeBean barcodeBean;
+
+    @EJB
+    private TitleDao titleDao;
+
+    @EJB
+    private BookDao bookDao;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null) {
+            User user = (User) session.getAttribute("user");
+            if(user.libraryStaff || user.libraryAdmin) {
+                if(request.getParameter("add-title-submit") != null) {
+                    String titleBarcode = request.getParameter("barcode"),
+                            title = request.getParameter("title"),
+                            authors = request.getParameter("authors");
+                    boolean initialCopy = request.getParameter("add-copy").equals("Yes");
+                    if(barcodeBean.isValidTitleBarcode(titleBarcode)) {
+                        if(titleDao.findByBarcode(titleBarcode) == null) {
+                            if(initialCopy) {
+                                String copy = request.getParameter("copy-barcode").isEmpty() ?
+                                        barcodeBean.generateCopyBarcode() : request.getParameter("copy-barcode");
+                                if(barcodeBean.isValidCopyBarcode(copy)) {
+                                    titleDao.create(new Title(titleBarcode, title, authors));
+                                    bookDao.create(new Book(copy, titleBarcode));
+                                } else {
+                                    session.setAttribute("status", SC_NOT_ACCEPTABLE);
+                                    response.sendRedirect("/library/add-title.jsp");
+                                }
+                            } else {
+                                titleDao.create(new Title(titleBarcode, title, authors));
+                            }
+                            session.setAttribute("status", SC_CREATED);
+                            response.sendRedirect("/library/add-title.jsp");
+                        } else {
+                            session.setAttribute("status", SC_CONFLICT);
+                            response.sendRedirect("/library/add-title.jsp");
+                        }
+                    } else {
+                        session.setAttribute("status", SC_NOT_ACCEPTABLE);
+                        response.sendRedirect("/library/add-title.jsp");
+                    }
+                } else {
+                    session.setAttribute("status", SC_BAD_REQUEST);
+                    response.sendRedirect("/library/add-title.jsp");
+                }
+            } else {
+                session.setAttribute("status", SC_FORBIDDEN);
+                response.sendRedirect("/library/dashboard.jsp");
+            }
+        } else {
+            session.setAttribute("status", SC_UNAUTHORIZED);
+            response.sendRedirect("/library/signin.jsp");
+        }
+    }
+}
